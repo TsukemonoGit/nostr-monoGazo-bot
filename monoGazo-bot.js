bcc2b0c201ca7fb3666a3e7c9950f2b3adb19436
TsukemonoGit <mine43try@outlook.com>
2023-08-05 15:36:33 +0900

    created_at+1

--- monoGazo-bot.js
+++ monoGazo-bot.js
@@ -1,13 +1,13 @@
 import 'websocket-polyfill'
-import { createRxNostr, createRxForwardReq, verify, uniq , now } from "rx-nostr";
+import { createRxNostr, createRxForwardReq, verify, uniq, now } from "rx-nostr";
 import env from "dotenv";
 env.config();
 import { urlList } from './imageList.js';
 
 
 
-const nsec=process.env.NSEC;
-const npub=process.env.PUBHEX;
+const nsec = process.env.NSEC;
+const npub = process.env.PUBHEX;
 const rxNostr = createRxNostr();
 await rxNostr.switchRelays(["wss://yabu.me"]);
 
@@ -15,13 +15,13 @@ const rxReq = createRxForwardReq();
 
 // Create observable
 const observable = rxNostr.use(rxReq)
-.pipe(
-  // Verify event hash and signature
-  verify(),
-  // Uniq by event hash
-  uniq(),
-  
-);
+  .pipe(
+    // Verify event hash and signature
+    verify(),
+    // Uniq by event hash
+    uniq(),
+
+  );
 
 //rxNostr.send({kind:1,content:"test",pubkey:npub},nsec);
 
@@ -29,38 +29,42 @@ const observable = rxNostr.use(rxReq)
 const subscription = observable.subscribe((packet) => {
   // Your minimal application!
   console.log(packet);
-  if(packet.event.content==="もの画像"||packet.event.content==="mono画像"){
-    const urlIndex = Math.floor(Math.random()*urlList.length);
-    console.log("ものがぞうりくえすときました:"+urlIndex);
-    const tags= [
-      ["p",packet.event.pubkey],
-      ["e",packet.event.id],
-      ["r",urlList[urlIndex].url],
-      ["t","もの画像"]];
+  const content = packet.event.content.trim();
+  if (content === "もの画像" || content === "mono画像") {
+    const urlIndex = Math.floor(Math.random() * urlList.length);
+    console.log("ものがぞうりくえすときました:" + urlIndex);
+    const tags = [
+      ["p", packet.event.pubkey],
+      ["e", packet.event.id],
+      ["r", urlList[urlIndex].url],
+      ["t", "もの画像"]];
 
-      const root = packet.event.tags.find((item) => item[item.length - 1] === "root");
+    const root = packet.event.tags.find((item) => item[item.length - 1] === "root");
 
-      // rootが見つかった場合、tagsにrootを追加
-      if (root) {
-        tags.push(root);
-      }
-    console.log(tags);
+    // rootが見つかった場合、tagsにrootを追加
+    if (root) {
+      tags.push(root);
+    }
+    console.log(packet.event.created_at + 1);
 
-    const res=rxNostr.send({
-      kind:packet.event.kind,
-      content:`#もの画像\n${urlList[urlIndex].url}\n作: nostr:${urlList[urlIndex].author} ${urlList[urlIndex].memo?" ("+urlList[urlIndex].memo+")":""}`,
-      tags:tags,
-      pubkey:npub,
-    },{seckey:nsec}).subscribe({
-      next:({from}) =>{
-        console.log("OK",from);
+    const res = rxNostr.send({
+      kind: packet.event.kind,
+      content: `#もの画像\n${urlList[urlIndex].url}\n作: nostr:${urlList[urlIndex].author} ${urlList[urlIndex].memo ? " (" + urlList[urlIndex].memo + ")" : ""}`,
+      tags: tags,
+      pubkey: npub,
+    }, {
+      created_at: packet.event.created_at + 1,
+      seckey: nsec
+    }).subscribe({
+      next: ({ from }) => {
+        console.log("OK", from);
       },
-      complete: ()=>{
+      complete: () => {
         console.log("Send complete");
       }
-  });
-}
+    });
+  }
 });
 
 // Send REQ message to listen kind1 events
-rxReq.emit({ kinds: [1,42] ,since:now });
+rxReq.emit({ kinds: [1, 42], since: now });
