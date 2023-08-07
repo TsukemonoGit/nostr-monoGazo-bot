95ebd8550371cf0230fe6c884a448038dcde1a72
TsukemonoGit <mine43try@outlook.com>
2023-08-07 22:42:37 +0900

    fix

--- monoGazo-bot.js
+++ monoGazo-bot.js
@@ -9,7 +9,7 @@ import { urlList } from './imageList.js';
 const nsec = process.env.NSEC;
 const npub = process.env.PUBHEX;
 const rxNostr = createRxNostr();
-await rxNostr.switchRelays(["wss://yabu.me", "wss://r.kojira.io"]);
+await rxNostr.switchRelays(["wss://yabu.me", "wss://r.kojira.io","wss://nostr.fediverse.jp"]);
 
 const rxReq = createRxForwardReq();
 
@@ -28,31 +28,74 @@ const observable = rxNostr.use(rxReq)
 // Start subscription
 const subscription = observable.subscribe((packet) => {
   // Your minimal application!
+  if (packet.event.pubkey === "f987fb90696fcb09358629aeebf5156ea05a405101c4f2d9020bf02f47ea4a49") { return; }
   console.log(packet);
   const content = packet.event.content.trim();
 
   if (content === "もの画像" || content === "mono画像") {
-    monoGazo(packet);
+    const urlIndex = Math.floor(Math.random() * urlList.length);
+    monoGazo(packet, urlIndex);
 
+  } else if (content.includes("ないんふぉふぉ") || content.includes("ないふぉふぉ")) {
+
+    naifofo(packet);
+
+    //-------------------------リプが来たとき
   } else if (content.includes("あるんふぉふぉ") || content.includes("あるふぉふぉ")) {
 
     profileChange(packet);
 
-    //-------------------------リプが来たとき
+
   } else if (packet.event.tags.some(item => item[0] === "p" && item.includes("f987fb90696fcb09358629aeebf5156ea05a405101c4f2d9020bf02f47ea4a49"))) {
     console.log("リプきたよ");
     // "comand"の部分を抽出
-    const command = packet.event.content.split(" ")[0];
-    switch (command) {
+    const commandList = packet.event.content.split(/[ 　]/);
+    const filteredCommands = commandList.filter(command => !command.startsWith("nostr:"));
+
+    switch (filteredCommands[0]) {
       case "和暦":
         wareki(packet);
         break;
-     
+      case "もの画像":
+      case "mono画像":
+        if (filteredCommands.length <= 1) {
+          const urlIndex = Math.floor(Math.random() * urlList.length);
+          monoGazo(packet, urlIndex);
+        } else if (filteredCommands[1] === "length" ||filteredCommands[1] === "長さ") {
+          monoLen(packet);
+        } else {
+          const numberValue = Number(filteredCommands[1]);
+          if (!isNaN(numberValue)) {
+            if (numberValue < urlList.length && numberValue >=0) {
+              monoGazo(packet, numberValue);
+            } else {
+              const tags = [
+                ["p", packet.event.pubkey],
+                ["e", packet.event.id]
+              ];
+              postEvent(packet.event.kind, "そんなのないよ", tags);
+             
+            }
+          }else{
+            const tags = [
+              ["p", packet.event.pubkey],
+              ["e", packet.event.id]
+            ];
+            postEvent(packet.event.kind, "そんなのないよ", tags);
+           
+          }
+        }
+        break;
+
       default:
-        console.log("switchのdefaultのとこ");
+        console.log("defaultのとこ");
+        const tags = [
+          ["p", packet.event.pubkey],
+          ["e", packet.event.id]
+        ];
+        postEvent(packet.event.kind, "₍ ･ᴗ･ ₎", tags);
         break;
     }
-
   }
 
 });
@@ -61,9 +104,10 @@ const subscription = observable.subscribe((packet) => {
 rxReq.emit({ kinds: [1, 42], since: now });
 
 
-//---------------------------------------------func
-function monoGazo(packet) {
-  const urlIndex = Math.floor(Math.random() * urlList.length);
+
+//---------------------------------------------func------------------------------------------------
+function monoGazo(packet, urlIndex) {
+
   console.log("ものがぞうりくえすときました:" + urlIndex);
   const tags = [
     ["p", packet.event.pubkey],
@@ -79,21 +123,8 @@ function monoGazo(packet) {
   }
   // console.log(packet.event.created_at + 1);
   const created_at = packet.event.created_at + 1;
+  postEvent(packet.event.kind, `#もの画像\n${urlList[urlIndex].url}\n作: nostr:${urlList[urlIndex].author} ${urlList[urlIndex].memo ? " (" + urlList[urlIndex].memo + ")" : ""}`, tags);
 
-  const res = rxNostr.send({
-    kind: packet.event.kind,
-    content: `#もの画像\n${urlList[urlIndex].url}\n作: nostr:${urlList[urlIndex].author} ${urlList[urlIndex].memo ? " (" + urlList[urlIndex].memo + ")" : ""}`,
-    tags: tags,
-    pubkey: npub,
-    //created_at:created_at
-  }, { seckey: nsec }).subscribe({
-    next: ({ from }) => {
-      console.log("OK", from);
-    },
-    complete: () => {
-      console.log("Send complete");
-    }
-  });
 
 }
 
@@ -111,44 +142,35 @@ function profileChange(packet) {
     displayName: "もの画像",
     banner: "",
     website: "",
-    about: "もの画像\nmono画像\n入れ残しとか\n入れないでとかあったら\nどうにかこうにかお伝え下さい",
+    about: "もの画像\nmono画像\nあるんふぉふぉ\n入れ残しとか\n入れないでとかあったら\nどうにかこうにかお伝え下さい",
     nip05: "",
     lud16: "",
     lud06: ""
   }
+  postEvent(0, JSON.stringify(metadata), []);
 
-  const res = rxNostr.send({
-    kind: 0,
-    content: JSON.stringify(metadata),
-    tags: [],
-    pubkey: npub,
-  }, {
-    seckey: nsec
-  }).subscribe({
-    next: ({ from }) => {
-      console.log("OK", from);
-    },
-    complete: () => {
-      console.log("Send complete");
-    }
-  });
+  postEvent(1, `あいこんかえた\n${urlList[urlIndex].url}\n作: nostr:${urlList[urlIndex].author} ${urlList[urlIndex].memo ? " (" + urlList[urlIndex].memo + ")" : ""}`, [["r", urlList[urlIndex].url]]);
 
-  const res2 = rxNostr.send({
-    kind: 1,
-    content: `あいこんかえた\n${urlList[urlIndex].url}\n作: nostr:${urlList[urlIndex].author} ${urlList[urlIndex].memo ? " (" + urlList[urlIndex].memo + ")" : ""}`,
-    tags: [["r", urlList[urlIndex].url]],
-    pubkey: npub,
-  }, {
-    created_at: packet.event.created_at + 1,
-    seckey: nsec
-  }).subscribe({
-    next: ({ from }) => {
-      console.log("OK", from);
-    },
-    complete: () => {
-      console.log("Send complete");
-    }
-  });
+}
+
+function naifofo(packet) {
+
+  console.log("ないらしい:");
+  const tags = [
+    ["p", packet.event.pubkey],
+    ["e", packet.event.id],
+    ["r", "https://cdn.nostr.build/i/84d43ed2d18e72aa9c012226628962c815d39c63374b446f7661850df75a7444.png"],
+    ["t", "もの画像"]];
+
+  const root = packet.event.tags.find((item) => item[item.length - 1] === "root");
+
+  // rootが見つかった場合、tagsにrootを追加
+  if (root) {
+    tags.push(root);
+  }
+  // console.log(packet.event.created_at + 1);
+  const created_at = packet.event.created_at + 1;
+  postEvent(packet.event.kind, `あるんふぉふぉどうぞ\nhttps://cdn.nostr.build/i/84d43ed2d18e72aa9c012226628962c815d39c63374b446f7661850df75a7444.png\n作: nostr:npub1e4qg56wvd3ehegd8dm7rlgj8cm998myq0ah8e9t5zeqkg7t7s93q750p76\n#もの画像`, tags);
 
 }
 
@@ -170,13 +192,44 @@ function wareki(packet) {
   const now = new Date();
   const wareki = now.toLocaleString("ja-JP-u-ca-japanese", { dateStyle: "long" });
 
+  postEvent(packet.event.kind, `${wareki} らしい`, tags,created_at);
 
+}
+
+function monoLen(packet) {
+  console.log("もの画像の数知りたいらしい");
+  const tags = [
+    ["p", packet.event.pubkey],
+    ["e", packet.event.id]];
+
+  const root = packet.event.tags.find((item) => item[item.length - 1] === "root");
+
+  // rootが見つかった場合、tagsにrootを追加
+  if (root) {
+    tags.push(root);
+  }
+  // console.log(packet.event.created_at + 1);
+  const created_at = packet.event.created_at + 1;
+
+  postEvent(packet.event.kind, `もの画像は今全部で${urlList.length-1}枚あるよ`, tags);
+}
+
+
+//-----------------------------
+// interface EventData {
+//   kind: Number,
+//   tags: string[][],
+//   created_at?: Number,
+//   content: string
+// }
+
+const postEvent=(kind, content, tags, created_at)=> {//}:EventData){
   const res = rxNostr.send({
-    kind: packet.event.kind,
-    content: `${wareki} らしい`,
+    kind: kind,
+    content: content,
     tags: tags,
     pubkey: npub,
-    //created_at:created_at
+    created_at:created_at 
   }, { seckey: nsec }).subscribe({
     next: ({ from }) => {
       console.log("OK", from);
