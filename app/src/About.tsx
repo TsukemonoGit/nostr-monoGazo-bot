0cd3064a7047ea7147fb893a79426607a20abd5a
TsukemonoGit <mine43try@outlook.com>
2023-09-15 20:54:15 +0900

    about

--- app/src/About.tsx
+++ app/src/About.tsx
@@ -1,16 +1,46 @@
 import { Component } from "solid-js"
 import styles from './About.module.css';
+import { css } from "../styled-system/css";
 
 
 
 const App: Component = () => {
     return (<>
-    <div class={styles.background}/>
-   
-    <h1 class={styles.title}>About</h1>
-    <div>
-        ここにボットのコマンドの情報とかなんとかが書かれる予定
-        
+        <div class={styles.background} />
+
+        <h1 class={styles.title}>About</h1>
+        <div >
+            もの画像BOTのコマンドの情報とかなんとか
+            <div class={styles.grid}>
+                <div class={styles.gridItem}>反応ワード</div>
+                <div class={styles.gridItem}>条件</div>
+                <div class={styles.gridItem}>説明</div>
+
+            <hr/><hr/><hr/>
+
+                <div class={styles.gridItem}>もの画像</div>
+                <div class={styles.gridItem}>完全一致</div>
+                <div class={styles.gridItem}>もの画像リストの中から一枚リプライします</div>
+
+            
+
+                <div class={styles.gridItem}>あるふぉふぉ<br />あるんふぉふぉ</div>
+                <div class={styles.gridItem}>が含まれる</div>
+                <div class={styles.gridItem}>もの画像のアイコンが変わる<br />嬉しいので</div>
+
+                
+
+                <div class={styles.gridItem}>ないふぉふぉ<br />ないんふぉふぉ</div>
+                <div class={styles.gridItem}>が含まれる</div>
+                <div class={styles.gridItem}>あるふぉふぉがもらえる</div>
+
+                <div class={styles.gridItem}>もの画像 [number]</div>
+                <div class={styles.gridItem}>リプライ</div>
+                <div class={styles.gridItem}>もの画像リストの[number]番の画像をリプライします（0始まり）</div>
+
+
+
+            </div>
         </div>
     </>
     )
