cbc2c74acba9aced996a23a861fd057d3a0b9f0b
TsukemonoGit <mine43try@outlook.com>
2023-09-26 22:22:05 +0900

    ボタンのスタイル

--- app/src/Contact.tsx
+++ app/src/Contact.tsx
@@ -6,36 +6,36 @@ import { css } from "../styled-system/css";
 
 
 const App: Component = () => {
-    return (<>
+  return (<>
     <h1 class={styles.title}>ほか</h1>
     <h3 class={styles.h3}>リンク</h3>
-    <a class={styles.link} href="https://nostter.vercel.app/mono_gazo@tsukemonoGit.github.io" target="_blank" // 新しいタブでリンクを開く設定
+    <div class={styles.item}><a class={styles.link} href="https://nostter.vercel.app/mono_gazo@tsukemonoGit.github.io" target="_blank" // 新しいタブでリンクを開く設定
       rel="noopener noreferrer" // セキュリティのために追加 recommended
- >もの画像BOT</a>
+    >もの画像BOT</a></div>
 
-    <a class={styles.link} href="https://github.com/TsukemonoGit/nostr-monoGazo-bot" target="_blank" // 新しいタブでリンクを開く設定
+    <div class={styles.item}><a class={styles.link} href="https://github.com/TsukemonoGit/nostr-monoGazo-bot" target="_blank" // 新しいタブでリンクを開く設定
       rel="noopener noreferrer" // セキュリティのために追加 recommended
- >このページのソースコード</a>
+    >このページのソースコード</a>  </div>
 
-    <a class={styles.link} href="https://nostter.vercel.app/mono@tsukemonogit.github.io" target="_blank" // 新しいタブでリンクを開く設定
+    <div class={styles.item}><a class={styles.link} href="https://nostter.vercel.app/mono@tsukemonogit.github.io" target="_blank" // 新しいタブでリンクを開く設定
       rel="noopener noreferrer" // セキュリティのために追加 recommended
- >作者（mono）</a>
+    >作者（mono）</a>    </div>
 
     <h3 class={styles.h3}>作ったNostrのツールたち</h3>
-    <div  class={css({display:'flex'})}>
-        <a class={styles.link} href="https://nostr-bookmark-viewer3.vercel.app/" target="_blank" // 新しいタブでリンクを開く設定
-      rel="noopener noreferrer" // セキュリティのために追加 recommended
- >ぶくまびうあ</a>
-       <a class={styles.link} href="https://dupstr.vercel.app/" target="_blank" // 新しいタブでリンクを開く設定
-      rel="noopener noreferrer" // セキュリティのために追加 recommended
- >イベント単品で他リレーに複製するやつ</a>
+    
+      <div class={styles.item}><a class={styles.link} href="https://nostr-bookmark-viewer3.vercel.app/" target="_blank" // 新しいタブでリンクを開く設定
+        rel="noopener noreferrer" // セキュリティのために追加 recommended
+      >ぶくまびうあ</a></div>
+      <div class={styles.item}><a class={styles.link} href="https://dupstr.vercel.app/" target="_blank" // 新しいタブでリンクを開く設定
+        rel="noopener noreferrer" // セキュリティのために追加 recommended
+      >イベント単品で他リレーに複製するやつ</a></div>
+    
+    <div class={css({ marginTop: '6rem' })}>
+      <p class={css({ marginTop: '10' })}>//管理者のためだけの画像追加用のやつ</p>
+      <MakeAddJson />
     </div>
-    <div class={css({marginTop:'6rem'})}>
-    <p class={css({marginTop:'10'})}>//管理者のためだけの画像追加用のやつ</p>
-    <MakeAddJson/>
-    </div>
-    </>
-    )
+  </>
+  )
 }
 
 export default App;
