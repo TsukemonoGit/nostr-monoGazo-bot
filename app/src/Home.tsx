3f79d4636a6f598e7dacb86da9fa808bc5bfea2c
TsukemonoGit <mine43try@outlook.com>
2024-01-27 15:40:04 +0900

    add monomoji no link

--- app/src/Home.tsx
+++ app/src/Home.tsx
@@ -12,11 +12,18 @@ const App: Component = () => {
           <img class={css({ height: "1.5em", width: 'auto', display: 'flex', marginRight: '2' })} src='/nostr-monoGazo-bot/images/2.png' alt='₍ ･ᴗ･ ₎' /> monoGazo List
 
         </h1>
-        <p class={css({ paddingBottom: 2 })}>nostrの もの画像BOT<a target="_blank"
+        <p class={css({ paddingBottom: 2 })}>nostrの <a class={css({ textDecoration: 'underline' })} target="_blank"
           rel="noopener noreferrer" href={`https://nostter.app/npub1lxrlhyrfdl9sjdvx9xhwhag4d6s95sz3q8z09kgzp0cz73l2ffys9p726u`}>
-          <svg class={styles.linkIcon} xmlns="http://www.w3.org/2000/svg" height="16" viewBox="0 -960 960 960" width="16">
+          もの画像BOT<svg class={styles.linkIcon} xmlns="http://www.w3.org/2000/svg" height="16" viewBox="0 -960 960 960" width="16">
             <path d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h280v80H200v560h560v-280h80v280q0 33-23.5 56.5T760-120H200Zm188-212-56-56 372-372H560v-80h280v280h-80v-144L388-332Z" fill="#0000FF" />
           </svg></a>が集めた画像たち</p>
+
+        <p class={css({ paddingBottom: 2 })}><a class={css({ textDecoration: 'underline' })} target="_blank"
+          rel="noopener noreferrer" href={`https://nostviewstr.vercel.app/naddr1qqyx6mmwdakk76nfqgsgfvxyd2mfntp4avk29pj8pwz7pqwmyzrummmrjv3rdsuhg9mc9agrqsqqqafnn3n5r5`}>
+          ものもじリスト<svg class={styles.linkIcon} xmlns="http://www.w3.org/2000/svg" height="16" viewBox="0 -960 960 960" width="16">
+            <path d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h280v80H200v560h560v-280h80v280q0 33-23.5 56.5T760-120H200Zm188-212-56-56 372-372H560v-80h280v280h-80v-144L388-332Z" fill="#0000FF" />
+          </svg></a>もあるよ</p>
+
         {jsonData && jsonData.length > 0 ? (
           <div class={styles.imageList}>
             {jsonData.map((item, index) => (
