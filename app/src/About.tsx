b907bbfc9ae4f06b167866e9ac3ef0ef877638e8
TsukemonoGit <mine43try@outlook.com>
2024-03-21 11:29:07 +0900

    mono command fix

--- app/src/About.tsx
+++ app/src/About.tsx
@@ -15,7 +15,7 @@ const App: Component = () => {
             <div >
                 <p class={css({ backgroundColor: 'rgba(255, 255, 255, 0.5)' })}>もの画像BOTのコマンドの情報とかなんとか</p>
                 <p class={css({ backgroundColor: 'rgba(255, 255, 255, 0.5)' })}>もの画像BOTが起動したときに₍ ･ᴗ･ ₎がでます</p>
-
+                <p class={css({ backgroundColor: 'rgba(255, 255, 255, 0.5)' })}>ものの部分はmonoでもOk</p>
                 <div class={styles.grid}>
                     <div class={styles.gridItem}>反応ワード</div>
                     <div class={styles.gridItem}>条件</div>
@@ -23,11 +23,10 @@ const App: Component = () => {
 
                     <hr /><hr /><hr />
 
-                    <div class={styles.gridItem}>もの画像
-                        <hr class={styles.hr} />
-                        mono画像</div>
+                    <div class={styles.gridItem}>もの画像</div>
+                        
                     <div class={styles.gridItem}>完全一致</div>
-                    <div class={styles.gridItem}>もの画像リストの中から一枚リプライします</div>
+                    <div class={styles.gridItem}>もの画像リストの中から一枚リプライ</div>
 
 
 
@@ -45,25 +44,67 @@ const App: Component = () => {
                     <div class={styles.gridItem}>が含まれる</div>
                     <div class={styles.gridItem}>あるふぉふぉがもらえる</div>
 
-                    <div class={styles.gridItem}>もの画像　[number]
-                        <hr class={styles.hr} />
-                        mono画像 [number]</div>
+                    <div class={styles.gridItem}>もの画像　[number]</div>
                     <div class={styles.gridItem}>リプライ</div>
-                    <div class={styles.gridItem}>もの画像リストの[number]番の画像をリプライします（0始まり）</div>
+                    <div class={styles.gridItem}>もの画像リストの[number]番の画像をリプライ（0始まり）</div>
+
+                    <div class={styles.gridItem}>ある（ん）ふぉふぉください</div>
+                    <div class={styles.gridItem}>リプライ</div>
+                    <div class={styles.gridItem}>あるんふぉふぉがもらえる</div>
 
                     <div class={styles.gridItem}>[nostr:npub~] に<br />ある(ん)ふぉふぉ(を)送って</div>
                     <div class={styles.gridItem}>リプライ</div>
-                    <div class={styles.gridItem}>[nostr:npub~]さんにあるふぉふぉをおくります</div>
+                    <div class={styles.gridItem}>[nostr:npub~]さんにあるふぉふぉをおくる</div>
 
-                    <div class={styles.gridItem}>もの画像 長さ
-                        <hr class={styles.hr} />
-                        mono画像 枚数</div>
+                    <div class={styles.gridItem}>ある（ん）ふぉふぉあげて</div>
                     <div class={styles.gridItem}>リプライ</div>
-                    <div class={styles.gridItem}>現在のもの画像の総数をリプライします</div>
+                    <div class={styles.gridItem}>TLにあるんふぉふぉを放流する</div>
+
+                    <div class={styles.gridItem}>もの画像 枚数
+                    </div>
+                    <div class={styles.gridItem}>リプライ</div>
+                    <div class={styles.gridItem}>現在のもの画像の総数をリプライ</div>
 
                     <div class={styles.gridItem}>和暦</div>
                     <div class={styles.gridItem}>リプライ</div>
-                    <div class={styles.gridItem}>和暦で今日の日付を教えてくれるよ<br />便利だね</div>
+                    <div class={styles.gridItem}>和暦で今日の日付を教えてくれる<br />便利だね</div>
+
+                    <div class={styles.gridItem}>もの画像どこ？</div>
+                    <div class={styles.gridItem}></div>
+                    <div class={styles.gridItem}>もの画像Botがでてくる</div>
+
+                    <div class={styles.gridItem}>ものサイトどこ？</div>
+                    <div class={styles.gridItem}></div>
+                    <div class={styles.gridItem}>このサイトのURLをリプライ</div>
+                </div>
+            </div>
+            <div class={css({ marginTop:"2rem" ,padding:"2rem"})}>
+                <p class={css({ backgroundColor: 'rgba(255, 255, 255, 0.5)' })}>(以下管理者限定コマンド)</p>
+               
+
+                <div class={styles.grid}>
+                    <div class={styles.gridItem}>反応ワード</div>
+                    <div class={styles.gridItem}>条件</div>
+                    <div class={styles.gridItem}>説明</div>
+
+                    <hr /><hr /><hr />
+
+                    <div class={styles.gridItem}>{"追加 {(追加するURLなど)}"}
+                       </div>
+                    <div class={styles.gridItem}>リプライ</div>
+                    <div class={styles.gridItem}>もの画像リストに追加</div>
+
+
+
+                    <div class={styles.gridItem}>削除 [number]</div>
+                    <div class={styles.gridItem}>リプライ</div>
+                    <div class={styles.gridItem}>もの画像リストから削除</div>
+
+                    <div class={styles.gridItem}>p [number] [comment]</div>
+                    <div class={styles.gridItem}></div>
+                    <div class={styles.gridItem}>pointlog.jsonに保存され、現在のポイント獲得数をreactionする</div>
+
+                 
                 </div>
             </div>
 
